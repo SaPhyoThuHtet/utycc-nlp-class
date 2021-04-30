14f84fff4e9528ce37ad8d3ea17ed82c1f4b1cba
Sa Phyo Thu Htet <71957964+SaPhyoThuHtet@users.noreply.github.com>
2021-04-30 10:22:52 +0630

    Add files via upload

---
+++ exe-4/MyanmarToBrailleConverter-pth.pl
@@ -0,0 +1,114 @@
+#!usr/bin/perl
+# Convert the Myanmar Words to Braille (Mu Haung)
+# Mu Haung - the very first style of writing burmese characters 
+
+# Phyo Thu Htet, Information Science Student, UTYCC
+# @SoftwareLab, UTYCC
+# 7 Sep 2019, Last Modified : 29 Oct 2019
+
+
+use strict;
+use warnings;
+use utf8;
+
+binmode(STDIN, ":utf8");
+binmode(STDOUT, ":utf8");
+binmode(STDERR, ":utf8");
+
+# Burmese => Braille Dictionary
+# The data for this dicitonary is prepared by Phyo Thu Htet, Naing Linn Phyo and Thiha Nyein
+my %braille_dict_mm = (
+                        "က"	=> "⡁",
+                        "ခ" 	=> "⢈",
+                        "ဂ"	=> "⠛",
+                        "ဃ"	=> "⠟",
+                        "င"	=> "⡈",
+                        "စ"	=> "⡌",
+                        "ဆ"    => "⡤",
+                        "ဇ"	=> "⠵",
+                        "ဈ"	=> "⣌",
+                        "ည"    => "⠷",
+                        "ဋ"	=> "⠳",
+                        "ဌ"     => "⠻",
+                        "ဍ"	=> "⠾",
+                        "ဎ"     => "⠿",
+                        "ဏ"	=> "⡬",
+                        "တ"    => "⠞",
+                        "ထ"    => "⠚",
+                        "ဒ"	=> "⠙",
+                        "ဓ"     => "⠋",
+                        "န"     => "⠝",
+                        "ပ"	=> "⡖",
+			"ဖ"	=> "⠰",
+			"ဗ"	=> "⢉",
+			"ဘ"	=> "⠃",
+			"မ"	=> "⡉",
+			"ယ"	=> "⠽",
+			"ရ"	=> "⠗",
+			"လ"	=> "⠇",
+			"ဝ"	=> "⠺",
+			"သ"	=> "⠹",
+			"ဟ"	=> "⠓",
+			"ဠ"	=> "⠸",
+			"အ"	=> "⠣",
+			"ဉ"	=> "⠧",
+                        "ဤ"    => "⠰⠪",
+			"၍"	=> "⠯",
+			"၏"	=> "⠕",
+			"၌"	=> "⠦",
+			"၎င်း"	=> "⠬",
+			"၊"	=> "?",
+			"။"	=> "?",
+                        "ာ"	=> "⠁", 
+                        "ါ"	=> "⠎", 
+                        "ိ"	=> "⠊", 
+                        "ီ"	=> "⠪", 
+                        "ု"	=> "⠑", 
+                        "ူ"	=> "⠥", 
+                        "ေ"	=> "⠱", 
+                        "ဲ"	=> "⠡", 
+                        "?"	=> "⠴",
+                        "ံ"     => "⠉", 
+                        "င်္" 	=> "⡈⠄⠤", 
+                        "ျ"	=> "⠔", 
+                        "ဥ"     => "⠰⠑ ",
+		        "ဦး"     => "⠰⠑⠪⠆", 
+          	        "ဧ"     => "⠰⠱",
+		        "ဣ"    => "⠰⠊", 
+                        "ြ"	=> "⠢", 
+                        "်"	=> "⠄", 
+                        "ွ" 	=> "⠜", 
+                        "ှ" 	=> "⠭", 
+                        "့" 	=> "⠂",
+                        "္"     => "⠤",
+                        "း"    => "⠆",                  
+                        );
+
+# Read a file
+open (my $inputFILE,"<:encoding(utf8)", $ARGV[0]) or die "Couldn't open input file $ARGV[0]!, $!\n";
+
+while (!eof($inputFILE)) {
+
+    my $input_file = <$inputFILE>;
+    chomp($input_file);
+    print "Input: $input_file\n";
+
+    # Change to Mu Haung Style
+    $input_file =~ s/([က-အ])([ါ-ူ]|[က-အ]်)/$1အ$2/g;
+    $input_file =~ s/([က-အ]([ျ-ှ]){1,})/$1အ/g;
+    print "Mu Haung : $input_file\n";
+
+    # Back to normal
+    $input_file =~  s/([က-အ][ျ-ှ]{1,})အ/$1/g;
+    $input_file =~  s/([က-အ])အ([ါ-ူ]|([က-အ])[က-အ]်)/$1$2/g;
+    print "Back to Normal : $input_file\n";
+    
+    # Switch to braille
+    while( my($find, $replace) = each %braille_dict_mm){
+        $input_file =~ s/($find)/$replace/g;
+    }
+    print "Braille:$input_file\n\n";
+
+}
+close ($inputFILE);
+
